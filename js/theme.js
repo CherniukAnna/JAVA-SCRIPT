5d0f4ae8f51edaa2a34cb9e809d28ccf8967ff45
CherniukAnna <chernukanna@gmail.com>
2023-08-15 21:29:02 +0300

    theme

---
+++ js/theme.js
@@ -0,0 +1,26 @@
+const themeSwitch = document.querySelector("#theme-switch");
+getCurrentTheme();
+function changeTheme(event) {
+    const isChecked = event.currentTarget.checked;
+
+    if (isChecked) {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+        localStorage.setItem("theme","dark")
+    } else {
+         document.body.classList.add("light");
+        document.body.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+    }
+}
+
+function getCurrentTheme() {
+    const currentTheme = localStorage.getItem("theme") || "light"
+    document.body.className = currentTheme;
+
+    if (currentTheme === "dark") {
+      themeSwitch.checked = true;
+    }
+}
+
+themeSwitch.addEventListener("change", changeTheme);
